ca66a45591ddce354721e94b16ee90a4bd0ca75f
Dan Cunnington <dancunnington@uk.ibm.com>
2023-06-07 21:26:52 +0100

    0.1 and 0.2 correct training sets

--- dan/playing_cards/create_reduced_perturbed_data.py
+++ dan/playing_cards/create_reduced_perturbed_data.py
@@ -5,7 +5,7 @@ import copy
 from os.path import join
 
 dataset_per_card_sizes = [64, 40, 10, 1]
-pct_images_correct = [1.0, 0.8, 0.6, 0.4]
+pct_images_correct = [0.2, 0.1]
 ALL_CARDS = ['2h', '2s', '2c', '2d', '3h', '3s', '3c', '3d', '4h', '4s', '4c', '4d', '5h', '5s', '5c',
                  '5d', '6h', '6s', '6c', '6d', '7h', '7s', '7c', '7d', '8h', '8s', '8c', '8d', '9h', '9s',
                  '9c', '9d', '10h', '10s', '10c', '10d', 'jh', 'js', 'jc', 'jd', 'qh', 'qs', 'qc', 'qd',
