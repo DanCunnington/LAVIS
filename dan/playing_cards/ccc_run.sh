7afde97bd603a5695544c758b938177783cc397f
Dan Cunnington <dancunnington@uk.ibm.com>
2023-06-07 12:46:03 +0100

    double quotes in bash string

--- dan/playing_cards/ccc_run.sh
+++ dan/playing_cards/ccc_run.sh
@@ -23,13 +23,13 @@ cd /u/$USER/lavis_runs/LAVIS_$FILENAME
 FILENAME="${NUM_EX}_ex_${PCT_CORRECT}_correct"
 DATASET_CONFIG="playing_cards_vqa_${FILENAME}.yaml"
 cp lavis/configs/datasets/playing_cards/playing_cards_vqa_template.yaml lavis/configs/datasets/playing_cards/${DATASET_CONFIG}
-sed -i -e 's/<<FILENAME>>/train_${FILENAME}.json/g' lavis/configs/datasets/playing_cards/${DATASET_CONFIG}
+sed -i -e "s/<<FILENAME>>/train_${FILENAME}.json/g" lavis/configs/datasets/playing_cards/${DATASET_CONFIG}
 
 
 # Create new projects/blip/train config with custom output dir
 BLIP_CONFIG="vqav2_playing_cards_${FILENAME}.yaml"
 cp lavis/projects/blip/train/vqav2_playing_cards_template.yaml lavis/projects/blip/train/$BLIP_CONFIG
-sed -i -e 's/<<FILENAME>>/${FILENAME}/g' lavis/projects/blip/train/$BLIP_CONFIG 
+sed -i -e "s/<<FILENAME>>/${FILENAME}/g" lavis/projects/blip/train/$BLIP_CONFIG 
 
 
 source activate ilasp_python
